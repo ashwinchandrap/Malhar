f22525583f6c56616fe0715838d8cd3058928f4c
Ashwin Chandra Putta <ashwin@datatorrent.com>
2014-02-07 19:54:26 -0800

    filter operator changes

--- apps/src/main/java/com/datatorrent/apps/logstream/FilterOperator.java
+++ apps/src/main/java/com/datatorrent/apps/logstream/FilterOperator.java
@@ -13,6 +13,7 @@ package com.datatorrent.apps.logstream;
  *  All Rights Reserved.
  */
 import com.datatorrent.api.*;
+import com.datatorrent.api.Context.OperatorContext;
 import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,208 +31,274 @@ import com.datatorrent.apps.logstream.PropertyRegistry.PropertyRegistry;
 import com.datatorrent.lib.util.KryoSerializableStreamCodec;
 import com.google.common.collect.Sets;
 import java.util.*;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Ashwin Chandra Putta <ashwin@datatorrent.com>
  */
-public class FilterOperator extends BaseOperator implements Partitionable<FilterOperator>
+public class FilterOperator extends BaseOperator
 {
+  private static final org.slf4j.Logger logger = LoggerFactory.getLogger(FilterOperator.class);
   /**
+   * key: type
+   * value --> map of
    * key: condition expression
    * value: list of keys on which the condition is
    */
-  private transient HashMap<String, String[]> conditions = new HashMap<String, String[]>();
-  private transient HashMap<String, Object> filterTuple;
-  @InputPortFieldAnnotation(name = "input")
-  private transient static PropertyRegistry<String> registry;
+  private HashMap<Integer, Map<String, String[]>> conditionList = new HashMap<Integer, Map<String, String[]>>();
+  private transient HashMap<String, ExpressionEvaluator> evaluators = new HashMap<String, ExpressionEvaluator>();
+  private static PropertyRegistry<String> registry;
 
   public void setRegistry(PropertyRegistry<String> registry)
   {
-    this.registry = registry;
+    FilterOperator.this.registry = registry;
   }
+
+  @Override
+  public void setup(OperatorContext context)
+  {
+    super.setup(context);
+    registry.bind("FILTER", "NO_CONDITION"); // used to represent the output tuple on which no condition is applied
+  }
+
+  @InputPortFieldAnnotation(name = "input")
   public final transient DefaultInputPort<Map<String, Object>> input = new DefaultInputPort<Map<String, Object>>()
   {
     @Override
     public void process(Map<String, Object> map)
     {
-      filterTuple = new HashMap<String, Object>(map);
-      ArrayList<String> filterList = new ArrayList<String>();
-      String[] filterArray = new String[conditions.size()];
-
-      for (String condition : conditions.keySet()) {
-        if (evaluate(condition, map)) {
-          filterList.add(condition);
+      HashMap<String, Object> filterTuple;
+      int typeId = (Integer)map.get("LOG_TYPE");
+      Object resp = map.get("response");
+      Map<String, String[]> conditions = conditionList.get(typeId);
+      if (conditions != null) {
+        for (String condition : conditions.keySet()) {
+          if (evaluate(condition, map, conditions.get(condition))) {
+            int index = registry.getIndex("FILTER", condition);
+            filterTuple = new HashMap<String, Object>(map);
+            filterTuple.put("FILTER", index);
+            outputMap.emit(filterTuple);
+          }
         }
       }
-      filterTuple.put("FILTER_LIST", filterList);
-      outputMap.emit(filterTuple);
+
+      // emit the same tuple once without any condition
+      int index = registry.getIndex("FILTER", "NO_CONDITION");
+      map.put("FILTER", index);
+      outputMap.emit((HashMap<String, Object>)map);
+    }
+
+    private boolean evaluate(String condition, Map<String, Object> map, String[] keys)
+    {
+      boolean ret = false;
+
+      //String[] keys = conditions.get(condition);
+      Object[] values = new Object[keys.length];
+      Class[] classTypes = new Class[keys.length];
+      ExpressionEvaluator ee = evaluators.get(condition);
+
+      try {
+        for (int i = 0; i < keys.length; i++) {
+          Object val = map.get(keys[i]);
+          //Object val = new String("404");
+        /*
+           if (((String)val).compareTo("404") == 0) {
+           logger.info("EQUALSSSSS");
+           }
+           char[] valCharArr = val.toString().toCharArray();
+           logger.info("value is {} and length is {} bytes are {}", val, val.toString().length(), val.toString().getBytes());
+           for (Character elem : valCharArr) {
+           System.out.print(elem + " | ");
+
+           }
+           */
+          if (val == null) {
+            logger.info("filter key {} missing in input record {}", keys[i], map);
+            return ret;
+          }
+          else {
+            values[i] = val;
+            classTypes[i] = val.getClass();
+          }
+        }
+
+        if (ee == null) {
+          ee = new ExpressionEvaluator(condition, Boolean.class, keys, classTypes);
+          evaluators.put(condition, ee);
+        }
+
+
+
+        //ee = new ExpressionEvaluator(condition, Boolean.class, keys, classTypes);
+        //ExpressionEvaluator ee = new ExpressionEvaluator(condition, Boolean.class, keys, classTypes);
+        ret = (Boolean)ee.evaluate(values);
+        //Object evaluate = ee.evaluate(values);
+        logger.debug("expression evaluated to {} for expression: {} with key class types: {} keys: {} values: {}", ret, condition, Arrays.toString(classTypes), Arrays.toString(keys), Arrays.toString(values));
+
+      }
+      catch (CompileException ex) {
+        Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (ParseException ex) {
+        Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (ScanException ex) {
+        Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
+      }
+      catch (InvocationTargetException ex) {
+        Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+      return ret;
+
     }
 
     /**
      * Stream codec used for partitioning.
+     *
+     * @Override
+     * public Class<? extends StreamCodec<Map<String, Object>>> getStreamCodec()
+     * {
+     * return FilterOperatorStreamCodec.class;
+     * }
      */
-    @Override
-    public Class<? extends StreamCodec<Map<String, Object>>> getStreamCodec()
-    {
-      return FilterOperatorStreamCodec.class;
-    }
-
   };
 
-  private boolean evaluate(String condition, Map<String, Object> map)
-  {
-    boolean ret = false;
-
-    String[] keys = conditions.get(condition);
-    Object[] values = new Object[keys.length];
-    Class[] types = new Class[keys.length];
-
-    try {
-      for (int i = 0; i < keys.length; i++) {
-        values[i] = map.get(keys[i]);
-        types[i] = map.get(keys[i]).getClass();
-      }
-
-      ExpressionEvaluator ee = new ExpressionEvaluator(condition, boolean.class, keys, types);
-      ret = (Boolean)ee.evaluate(values);
-
-    }
-    catch (CompileException ex) {
-      Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    catch (ParseException ex) {
-      Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    catch (ScanException ex) {
-      Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    catch (InvocationTargetException ex) {
-      Logger.getLogger(FilterOperator.class.getName()).log(Level.SEVERE, null, ex);
-    }
-
-    return ret;
-
-  }
-
-  public void addCondition(String[] condition)
+  public void addFilterCondition(String[] condition)
   {
     // list of keys followed by the expressions, all entities separated by semi colon
-    //a,b,c,a==1&&b==2&&c=="abc"
+    //type=apache,a,b,c,a==1&&b==2&&c=="abc"
     //String[] split = condition.split(";");
 
-    String[] keys = new String[condition.length - 1];
-    System.arraycopy(condition, 0, keys, 0, keys.length);
+    String[] split = condition[0].split("=");
+    String type = split[1];
+    int typeId = registry.getIndex("LOG_TYPE", type);
+    String[] keys = new String[condition.length - 2];
 
+    System.arraycopy(condition, 1, keys, 0, keys.length);
+
+    for (String string : keys) {
+      System.out.println("condition keys = " + string);
+    }
     String expression = condition[condition.length - 1];
 
+    Map<String, String[]> conditions = conditionList.get(typeId);
+    if (conditions == null) {
+      conditions = new HashMap<String, String[]>();
+      conditionList.put(typeId, conditions);
+    }
+
     conditions.put(expression, keys);
+    if (registry != null) {
+      registry.bind("FILTER", expression);
+    }
   }
 
   //@OutputPortFieldAnnotation(name = "outputFilterMap")
   //public final transient DefaultOutputPort<HashMap<String, Map<String, Object>>> outputFilterMap = new DefaultOutputPort<HashMap<String, Map<String, Object>>>();
-
   @OutputPortFieldAnnotation(name = "outputMap")
   public final transient DefaultOutputPort<HashMap<String, Object>> outputMap = new DefaultOutputPort<HashMap<String, Object>>();
 
-  @Override
-  public Collection<Partition<FilterOperator>> definePartitions(Collection<Partition<FilterOperator>> partitions, int incrementalCapacity)
-  {
-    ArrayList<Partition<FilterOperator>> newPartitions = new ArrayList<Partition<FilterOperator>>();
-    //int partitionSize = LOG_TYPE.values().length;
-    String[] logTypes = registry.list("LOG_TYPE");
-    int partitionSize = logTypes.length;
-    int expectedPartitionSize = partitions.size() + incrementalCapacity;
-    if (expectedPartitionSize > partitionSize) {
-      partitionSize = expectedPartitionSize;
-    }
+  /*
+   @Override
+   public Collection<Partition<FilterOperator>> definePartitions(Collection<Partition<FilterOperator>> partitions, int incrementalCapacity)
+   {
+   ArrayList<Partition<FilterOperator>> newPartitions = new ArrayList<Partition<FilterOperator>>();
+   //int partitionSize = LOG_TYPE.values().length;
+   String[] logTypes = registry.list("LOG_TYPE");
+   int partitionSize = logTypes.length;l
+   int expectedPartitionSize;
+   if (partitions.size() == 1) {
+   // initial partitioning
+   expectedPartitionSize = partitionSize;
+   } else {
+   // TODO: figure out what to do in this scenerio
+   //expectedPartitionSize = partitionSize * 2;
+   expectedPartitionSize = partitionSize;
+   }
 
-    for (int i = 0; i < partitionSize; i++) {
-      FilterOperator filterOperator = new FilterOperator();
+   for (int i = 0; i < expectedPartitionSize; i++) {
+   FilterOperator filterOperator = new FilterOperator();
 
-      Partition<FilterOperator> partition = new DefaultPartition<FilterOperator>(filterOperator);
-      newPartitions.add(partition);
-    }
+   Partition<FilterOperator> partition = new DefaultPartition<FilterOperator>(filterOperator);
+   newPartitions.add(partition);
+   }
 
-    /*
-    int partitionBits = (Integer.numberOfLeadingZeros(0) - Integer.numberOfLeadingZeros(partitionSize - 1));
+   int partitionMask = -1; // all bits
 
-    int partitionMask = 0;
-    if (partitionBits > 0) {
-      partitionMask = -1 >>> (Integer.numberOfLeadingZeros(-1)) - partitionBits;
-    }
-    */
-    int partitionMask = -1; // all bits
+   for (int i=0; i <= newPartitions.size(); i++) {
+   Partition<FilterOperator> partition = newPartitions.get(i);
+   String partitionVal = logTypes[i % logTypes.length];
+   int key = registry.getIndex("LOG_TYPE", partitionVal);
+   partition.getPartitionKeys().put(input, new PartitionKeys(partitionMask, Sets.newHashSet(key)));
+   }
 
-    for (int i=0; i <= newPartitions.size(); i++) {
-      Partition<FilterOperator> partition = newPartitions.get(i);
-      String partitionVal = logTypes[i % logTypes.length];
-      int key = registry.getIndex("LOG_TYPE", partitionVal);
-      partition.getPartitionKeys().put(input, new PartitionKeys(partitionMask, Sets.newHashSet(key)));
-    }
+   return newPartitions;
 
-    return newPartitions;
-
-  }
+   }
+   */
 
   /* Default repartitioning should mostly be enough for repartitioning
-  @Override
-  public Response processStats(BatchedOperatorStats bos)
-  {
-    Response res = new Response();
+   @Override
+   public Response processStats(BatchedOperatorStats bos)
+   {
+   Response res = new Response();
 
-    //TODO: set res.repartitionRequired to true based on dynamic partitioning logic
-    // so that it calls definePartitions when its true
+   //TODO: set res.repartitionRequired to true based on dynamic partitioning logic
+   // so that it calls definePartitions when its true
 
-    return res;
+   return res;
 
-  }
-  */
-
-  public static class FilterOperatorStreamCodec extends KryoSerializableStreamCodec<Map<String, Object>>
-  {
-    @Override
-    public int getPartition(Map<String, Object> o)
-    {
-      /* LOGIC 1
-      int ret = 0;
-      //LOG_TYPE type = LOG_TYPE.valueOf(o.getClass().getSimpleName());
-      LOG_TYPE type = (LOG_TYPE)o.get("LOG_TYPE");
-
-      switch (type) {
-        case APACHE:
-          ret = 0;
-          break;
-        case MYSQL:
-          ret = 1;
-          break;
-        case SYS:
-          ret = 2;
-          break;
-        default: // APACHE
-          ret = 0;
-      }
-
-      return ret;
-      */
-
-      int ret;
-      String[] list = registry.list("LOG_TYPE");
-      if (list == null) {
-        return 0;
-      } else if (list.length == 0) {
-        return 0;
-      }
-
-      ret = registry.getIndex("LOG_TYPE", (String)o.get("LOG_TYPE"));
-      return ret;
-
-    }
-  }
+   }
+   */
 
   /*
-  enum LOG_TYPE {
-    APACHE, MYSQL, SYS
-  }
-  */
+   public static class FilterOperatorStreamCodec extends KryoSerializableStreamCodec<Map<String, Object>>
+   {
+   @Override
+   public int getPartition(Map<String, Object> o)
+   {
+   /* LOGIC 1
+   int ret = 0;
+   //LOG_TYPE type = LOG_TYPE.valueOf(o.getClass().getSimpleName());
+   LOG_TYPE type = (LOG_TYPE)o.get("LOG_TYPE");
+
+   switch (type) {
+   case APACHE:
+   ret = 0;
+   break;
+   case MYSQL:
+   ret = 1;
+   break;
+   case SYS:
+   ret = 2;
+   break;
+   default: // APACHE
+   ret = 0;
+   }
+
+   return ret;
+
+
+   int ret;
+   String[] list = registry.list("LOG_TYPE");
+   if (list == null) {
+   return 0;
+   } else if (list.length == 0) {
+   return 0;
+   }
+
+   ret = registry.getIndex("LOG_TYPE", (String)o.get("LOG_TYPE"));
+   return ret;
+
+   }
+   }
+   */
+
+  /*
+   enum LOG_TYPE {
+   APACHE, MYSQL, SYS
+   }
+   */
 }
--- apps/src/test/java/com/datatorrent/apps/logstream/FilterOperatorTest.java
+++ apps/src/test/java/com/datatorrent/apps/logstream/FilterOperatorTest.java
@@ -4,8 +4,16 @@
  */
 package com.datatorrent.apps.logstream;
 
+import com.datatorrent.api.*;
+import com.datatorrent.api.annotation.InputPortFieldAnnotation;
+import com.datatorrent.api.annotation.OutputPortFieldAnnotation;
+import com.datatorrent.apps.logstream.PropertyRegistry.LogstreamPropertyRegistry;
 import com.datatorrent.lib.testbench.CollectorTestSink;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.hadoop.conf.Configuration;
 import org.junit.Test;
 
 /**
@@ -14,29 +22,104 @@ import org.junit.Test;
  */
 public class FilterOperatorTest
 {
+  public static class TestApplication implements StreamingApplication
+  {
+    @Override
+    public void populateDAG(DAG dag, Configuration c)
+    {
+      LogstreamPropertyRegistry registry = new LogstreamPropertyRegistry();
+      registry.bind("LOG_TYPE", "apache");
+
+      TestInputOperator inputOperator = dag.addOperator("InputOperator", new TestInputOperator());
+      inputOperator.setRegistry(registry);
+
+      FilterOperator filterOperator = dag.addOperator("FilterOperator", new FilterOperator());
+      filterOperator.setRegistry(registry);
+      String val = "404";
+      filterOperator.addFilterCondition(new String[] {"type=apache", "response", "response.equals(\"404\")"});
+
+      TestOutputOperator outputOperator = dag.addOperator("OutputOperator", new TestOutputOperator());
+
+      dag.addStream("input", inputOperator.outMap, filterOperator.input);
+      dag.addStream("filter", filterOperator.outputMap, outputOperator.input);
+    }
+
+  }
+
+  public static class TestInputOperator extends BaseOperator implements InputOperator
+  {
+    private HashMap<String, Object> map;
+    private LogstreamPropertyRegistry registry;
+
+    public void setRegistry(LogstreamPropertyRegistry registry)
+    {
+      this.registry = registry;
+    }
+
+    @OutputPortFieldAnnotation(name = "outMap")
+    public final transient DefaultOutputPort<HashMap<String, Object>> outMap = new DefaultOutputPort<HashMap<String, Object>>();
+
+    @Override
+    public void emitTuples()
+    {
+      map = new HashMap<String, Object>();
+      map.put("LOG_TYPE", registry.getIndex("LOG_TYPE", "apache"));
+      String val = new String("404");
+      map.put("response", val);
+      outMap.emit(map);
+    }
+
+  }
+
+  public static class TestOutputOperator extends BaseOperator
+  {
+    @InputPortFieldAnnotation(name = "input")
+    public final transient DefaultInputPort<Map<String, Object>> input = new DefaultInputPort<Map<String, Object>>()
+    {
+      @Override
+      public void process(Map<String, Object> t)
+      {
+        System.out.println("tuple received ## " + t);
+      }
+
+    };
+  }
+
   @Test
-  public void testOperator() {
+  public void testOperator()
+  {
+    LocalMode.runApp(new TestApplication(), 60000);
 
-    FilterOperator oper = new FilterOperator();
+    /*
+     FilterOperator oper = new FilterOperator();
+     LogstreamPropertyRegistry registry = new LogstreamPropertyRegistry();
+     registry.bind("LOG_TYPE", "apache");
+     oper.setRegistry(registry);
+     oper.setup(null);
 
-    oper.addCondition(new String[]{"a","b","c","d","a==\"1\"&&b==\"2\"&&c==\"abc\""});
-    oper.addCondition(new String[]{"d","d>0"});
-    HashMap<String, Object> inMap = new HashMap<String, Object>();
-    inMap.put("a", "1");
-    inMap.put("b", "2");
-    inMap.put("c", "abc");
-    inMap.put("d", 1);
-    inMap.put("e", "3");
+     //oper.addFilterCondition(new String[]{"type=apache","a","b","c_info","d","a==\"1\"&&b==\"2\"&&c_info==\"abc\""});
+     //oper.addFilterCondition(new String[]{"type=apache","d","d==1"});
+     //oper.addFilterCondition(new String[]{"type=apache","a","a==\"1\""});
+     oper.addFilterCondition(new String[]{"type=apache", "response", "response==\"404\""});
+     HashMap<String, Object> inMap = new HashMap<String, Object>();
+     inMap.put("LOG_TYPE", registry.getIndex("LOG_TYPE", "apache"));
+     //inMap.put("a", "1");
+     //inMap.put("b", "2");
+     //inMap.put("c_info", "abc");
+     //inMap.put("d", 1);
+     //inMap.put("e", "3");
+     inMap.put("response", "404");
 
-    CollectorTestSink mapSink = new CollectorTestSink();
-    oper.outputMap.setSink(mapSink);
+     CollectorTestSink mapSink = new CollectorTestSink();
+     oper.outputMap.setSink(mapSink);
 
-    oper.beginWindow(0);
-    oper.input.process(inMap);
-    oper.endWindow();
+     oper.beginWindow(0);
+     oper.input.process(inMap);
+     oper.endWindow();
 
-    System.out.println(mapSink.collectedTuples);
-
+     System.out.println(mapSink.collectedTuples);
+     //System.out.println("registry = " + registry.toString());
+     */
 
   }
 
