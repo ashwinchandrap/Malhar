c4e3d0cc813dd34b6c509c4f21605a5bee1efd68
Ashwin Chandra Putta <ashwin@datatorrent.com>
2014-05-15 11:54:55 -0700

    SPOI-2494 #resolves added http get operator
    Squashed commit of the following:
    
    commit f1af83c92b431b7b9da7054a928b5a6b7392b692
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Thu May 15 11:46:24 2014 -0700
    
        clean up
    
    commit d66ce22bea481fa2f9d721828b3a3296e0960d28
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Thu May 15 11:34:17 2014 -0700
    
        added http get operator with map input port for query parameters
    
    commit 91b3895769f6b4476e8fe88a233bb746f15fc06d
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Mon May 12 19:16:33 2014 -0700
    
        clean up
    
    commit 9c108cd370d695d540dad2fddd9b520bbc840248
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Mon May 12 19:06:15 2014 -0700
    
        renamed HttpOutputOperatorTest to HttpPostOutputOperatorTest
    
    commit 173be15c017eff53fd426efadac6091169f5c926
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Mon May 12 18:44:41 2014 -0700
    
        changed method call to set the url
    
    commit c07d4b5c077405bbb1406f8be03f0ebdf852ce2a
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Mon May 12 18:44:03 2014 -0700
    
        added HttpGetOperator to send get requests and process responses if output port is connected
    
    commit 6620a1f3f730e936c3645e74411a89fb34cb1d30
    Author: Ashwin Chandra Putta <ashwin@datatorrent.com>
    Date:   Mon May 12 18:43:15 2014 -0700
    
        added abstract http operator and renamed HttpOutputOperator to HttpPostOutputOperator

--- library/src/main/java/com/datatorrent/lib/io/AbstractHttpOperator.java
+++ library/src/main/java/com/datatorrent/lib/io/AbstractHttpOperator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,16 +15,11 @@
  */
 package com.datatorrent.lib.io;
 
-import java.net.URI;
-import java.util.Map;
-
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
-import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,57 +29,36 @@ import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.annotation.ShipContainingJars;
 
 /**
- *
- * Sends tuple as POST with JSON content to the given URL<p>
- * <br>
- * Data of type {@link java.util.Map} is converted to JSON. All other types are sent in their {@link Object#toString()} representation.<br>
- * <br>
- *
- * @param <T>
- * @since 0.3.2
+ * Abstract http operator that creates client at setup and destroy at teardown
+ * and provides the process tuple method to be implemented to process each incoming tuple
+ * @param <T> 
  */
 @ShipContainingJars(classes = {com.sun.jersey.api.client.ClientHandler.class})
-public class HttpOutputOperator<T> extends BaseOperator
+public abstract class AbstractHttpOperator<T> extends BaseOperator
 {
-  private static final Logger LOG = LoggerFactory.getLogger(HttpOutputOperator.class);
-
+  @NotNull
+  protected String url;
+  protected transient Client wsClient;
+  protected transient WebResource resource;
   public final transient DefaultInputPort<T> input = new DefaultInputPort<T>()
   {
     @Override
     public void process(T t)
     {
-      if (t instanceof Map) {
-        resource.type(MediaType.APPLICATION_JSON).post(new JSONObject((Map<?, ?>)t).toString());
-      }
-      else {
-        resource.post(t.toString());
-      }
+      processTuple(t);
     }
+
   };
 
-  /**
-   * The URL of the web service resource for the POST request.
-   */
-  @NotNull
-  private URI resourceUrl;
-  private transient Client wsClient;
-  private transient WebResource resource;
-
-  public void setResourceURL(URI url)
-  {
-    if (!url.isAbsolute()) {
-      throw new IllegalArgumentException("URL is not absolute: " + url);
-    }
-    this.resourceUrl = url;
-  }
+  protected abstract void processTuple(T t);
 
   @Override
   public void setup(OperatorContext context)
   {
     wsClient = Client.create();
     wsClient.setFollowRedirects(true);
-    resource = wsClient.resource(resourceUrl); // side step "not absolute URL" after serialization
-    LOG.info("URL: {}", resourceUrl);
+    resource = wsClient.resource(url);
+    logger.info("URL: {}", url);
   }
 
   @Override
@@ -95,4 +69,16 @@ public class HttpOutputOperator<T> extends BaseOperator
     }
     super.teardown();
   }
+
+  public String getUrl()
+  {
+    return url;
+  }
+
+  public void setUrl(String url)
+  {
+    this.url = url;
+  }
+
+  private static final Logger logger = LoggerFactory.getLogger(AbstractHttpOperator.class);
 }
