93f5aa66a6e9aeae2006db197c37c7ae6a5cee62
Ashwin Chandra Putta <ashwin@datatorrent.com>
2014-05-19 17:36:37 -0700

    added json mapper operator

---
+++ library/src/main/java/com/datatorrent/lib/stream/JsonMapperOperator.java
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (c) 2012-2014 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.lib.stream;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datatorrent.api.BaseOperator;
+import com.datatorrent.api.Context.OperatorContext;
+import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.DefaultOutputPort;
+
+/**
+ *
+ */
+public class JsonMapperOperator<INPUT, OUTPUT> extends BaseOperator
+{
+  private transient ObjectMapper mapper;
+  private transient Class<OUTPUT> outputClass;
+  private String outputClassName = "java.util.HashMap"; // default output class
+  public final transient DefaultOutputPort<OUTPUT> output = new DefaultOutputPort<OUTPUT>();
+  public final transient DefaultInputPort<INPUT> input = new DefaultInputPort<INPUT>()
+  {
+    @Override
+    public void process(INPUT input)
+    {
+      if (input instanceof String || input instanceof byte[]) {
+        convertToObject(input.toString());
+      }
+      else {
+        convertToJson(input);
+      }
+    }
+
+  };
+
+  @SuppressWarnings("unchecked")
+  private void convertToObject(String input)
+  {
+    try {
+      OUTPUT readValue = mapper.readValue(input, outputClass);
+      output.emit(readValue);
+    }
+    catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void convertToJson(INPUT input)
+  {
+    try {
+      String jsonString = mapper.writeValueAsString(input);
+      output.emit((OUTPUT)jsonString);
+    }
+    catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public void setup(OperatorContext context)
+  {
+    try {
+      mapper = new ObjectMapper(new JsonFactory());
+      outputClass = (Class<OUTPUT>)Class.forName(outputClassName);
+    }
+    catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  public String getOutputClassName()
+  {
+    return outputClassName;
+  }
+
+  public void setOutputClassName(String outputClassName)
+  {
+    this.outputClassName = outputClassName;
+  }
+
+  private static final long serialVersionUID = 201405191115L;
+  private static final Logger logger = LoggerFactory.getLogger(JsonMapperOperator.class);
+}
---
+++ library/src/test/java/com/datatorrent/lib/stream/JsonMapperOperatorTest.java
@@ -0,0 +1,137 @@
+/*
+ *  Copyright (c) 2012-2014 Malhar, Inc.
+ *  All Rights Reserved.
+ */
+package com.datatorrent.lib.stream;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datatorrent.lib.testbench.CollectorTestSink;
+import java.io.IOException;
+import java.util.HashMap;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Assert;
+
+/**
+ *
+ * @author Ashwin Chandra Putta <ashwin@datatorrent.com>
+ */
+public class JsonMapperOperatorTest
+{
+  @Test
+  public void testJsonToObject() throws IOException {
+    JsonMapperOperator<String, MyPojo> oper = new JsonMapperOperator<String, MyPojo>();
+
+    //oper.setOutputClassName("com.datatorrent.lib.stream.JsonMapperOperatorTest.MyPojo.class");
+
+    oper.setup(null);
+    CollectorTestSink objectSink = new CollectorTestSink();
+    oper.output.setSink(objectSink);
+
+    ObjectMapper mapper = new ObjectMapper(new JsonFactory());
+
+    oper.beginWindow(0);
+
+    MyPojo pojo1 = new MyPojo();
+    pojo1.setName("country");
+    pojo1.setValue("usa");
+
+    // input test json string
+    String inputJson = mapper.writeValueAsString(pojo1);
+
+    // run the operator for the same string 1000 times
+    int numtuples = 1000;
+    for (int i = 0; i < numtuples; i++) {
+      oper.input.process(inputJson);
+    }
+
+    oper.endWindow();
+
+    // assert that the number of the operator generates is 1000
+    Assert.assertEquals("number emitted tuples", numtuples, objectSink.collectedTuples.size());
+
+    // assert that value for one of the keys in any one of the objects from mapSink is as expected
+    /*
+    MyPojo obj = (MyPojo)objectSink.collectedTuples.get(510);
+    Assert.assertEquals("emitted tuple", "country", obj.getName());
+    Assert.assertEquals("emitted tuple", "usa", obj.getValue());
+    */
+    HashMap<String, String> obj = (HashMap<String, String>)objectSink.collectedTuples.get(510);
+    Assert.assertEquals("emitted tuple", "country", obj.get("name"));
+    Assert.assertEquals("emitted tuple", "usa", obj.get("value"));
+  }
+
+  @Test
+  public void testObjectToJson() throws IOException {
+    JsonMapperOperator<MyPojo, String> oper = new JsonMapperOperator<MyPojo, String>();
+    oper.setup(null);
+
+    CollectorTestSink jsonStringSink = new CollectorTestSink();
+    oper.output.setSink(jsonStringSink);
+
+    ObjectMapper mapper = new ObjectMapper(new JsonFactory());
+
+    oper.beginWindow(0);
+
+    MyPojo pojo = new MyPojo();
+    pojo.setName("city");
+    pojo.setValue("sunnyvale");
+
+    // run the operator for the same string 1000 times
+    int numtuples = 1000;
+    for (int i = 0; i < numtuples; i++) {
+      oper.input.process(pojo);
+    }
+
+    oper.endWindow();
+
+    // assert that the number of the operator generates is 1000
+    Assert.assertEquals("number emitted tuples", numtuples, jsonStringSink.collectedTuples.size());
+
+    // assert that value for one of the keys in any one of the objects from mapSink is as expected
+    String jsonStr = (String)jsonStringSink.collectedTuples.get(510);
+    MyPojo convertedValue = mapper.readValue(jsonStr, MyPojo.class);
+
+    Assert.assertEquals("emitted tuple", "city", convertedValue.getName());
+    Assert.assertEquals("emitted tuple", "sunnyvale", convertedValue.getValue());
+  }
+
+  public static class MyPojo
+  {
+    String name;
+    String value;
+
+    public MyPojo()
+    {
+    }
+
+    public String getName()
+    {
+      return name;
+    }
+
+    public void setName(String name)
+    {
+      this.name = name;
+    }
+
+    public String getValue()
+    {
+      return value;
+    }
+
+    public void setValue(String value)
+    {
+      this.value = value;
+    }
+
+    @Override
+    public String toString()
+    {
+      return "MyPojo{" + "name=" + name + ", value=" + value + '}';
+    }
+  }
+}
